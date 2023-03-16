7b5465144c28aa9302289da42c857151b1f1fdd4
Kuba Marek <blue.cube@seznam.cz>
2023-03-16 10:37:33 +0100

    Refactored to a JS class

--- pathtrace.js
+++ pathtrace.js
@@ -2,51 +2,6 @@ function downloadFile(f) {
     return fetch(f).then(result => result.text());
 }
 
-function createShader(gl, type, source) {
-    const shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
-        throw Error('Shader compilation error: ' + gl.getShaderInfoLog(shader));
-    return shader;
-}
-
-function createProgram(gl, shaders) {
-    const program = gl.createProgram();
-    shaders.forEach(s => gl.attachShader(program, s));
-    gl.linkProgram(program);
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS))
-        throw Error('Program linking error: ' + gl.getProgramInfoLog(program));
-    return program;
-}
-
-/**
- * Create vertex positions buffer and assign it to `a_position` attribute.
- * Leaves the vertex position array buffer bound.
- */
-function createVertexPositions(gl, program) {
-    const vertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-        -1, -1, 0, 1,
-         1, -1, 0, 1,
-        -1,  1, 0, 1,
-         1,  1, 0, 1
-    ]), gl.STATIC_DRAW);
-    const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
-    gl.enableVertexAttribArray(positionAttributeLocation);
-    gl.vertexAttribPointer(positionAttributeLocation, 4, gl.FLOAT, false, 0, 0);
-}
-
-/**
- * Return a promise that takes the value of the #canvas element once the window is loaded.
- */
-function findCanvas() {
-    return new Promise((resolve, reject) => {
-        window.addEventListener("load", resolve)
-    }).then(() => document.getElementById("canvas"));
-}
-
 function crossProduct(v1, v2) {
     return [
         v1[1] * v2[2] - v1[2] * v2[1],
@@ -67,69 +22,120 @@ function scale(v, s) {
     return v.map(x => x * s);
 }
 
-/**
- * Calculate camera forward, up and right vectors from a forward vector.
- * Outputs are mutually perpendicular, camera is oriented going up along the Z axis.
- * Forward is scaled by forwardScale, up and right are by upRightScale.
- */
-function calculateCamera(forward, forwardScale, upRightScale) {
-    const right = normalize(crossProduct(forward, [0, 0, 1]));
-    const up = normalize(crossProduct(right, forward));
-    return [
-        scale(normalize(forward), forwardScale),
-        scale(up, upRightScale),
-        scale(right, upRightScale)
-    ];
+class Pathtrace {
+    createShader(type, source) {
+        const shader = this.gl.createShader(type);
+        this.gl.shaderSource(shader, source);
+        this.gl.compileShader(shader);
+        if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS))
+            throw Error('Shader compilation error: ' + this.gl.getShaderInfoLog(shader));
+        return shader;
+    }
+
+    createProgram(shaders) {
+        const program = this.gl.createProgram();
+        shaders.forEach(s => this.gl.attachShader(program, s));
+        this.gl.linkProgram(program);
+        if (!this.gl.getProgramParameter(program, this.gl.LINK_STATUS))
+            throw Error('Program linking error: ' + this.gl.getProgramInfoLog(program));
+        return program;
+    }
+
+    /**
+     * Create vertex positions buffer prepared for a triangle strip covering the whole viewport
+     * and assign it to `a_position` attribute.
+     * Leaves the vertex position array buffer bound.
+     */
+    createVertexPositions() {
+        const vertexBuffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, vertexBuffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array([
+            -1, -1, 0, 1,
+             1, -1, 0, 1,
+            -1,  1, 0, 1,
+             1,  1, 0, 1
+        ]), this.gl.STATIC_DRAW);
+        const positionAttributeLocation = this.gl.getAttribLocation(this.program, 'a_position');
+        this.gl.enableVertexAttribArray(positionAttributeLocation);
+        this.gl.vertexAttribPointer(positionAttributeLocation, 4, this.gl.FLOAT, false, 0, 0);
+    }
+
+    /**
+     * Calculate camera forward, up and right vectors from a forward vector.
+     * Outputs are mutually perpendicular, camera is oriented going up along the Z axis.
+     * Forward is scaled by forwardScale, up and right are by upRightScale.
+     */
+    static calculateCamera(forward, forwardScale, upRightScale) {
+        const right = normalize(crossProduct(forward, [0, 0, 1]));
+        const up = normalize(crossProduct(right, forward));
+        return [
+            scale(normalize(forward), forwardScale),
+            scale(up, upRightScale),
+            scale(right, upRightScale)
+        ];
+    }
+
+    /**
+     * Return a promise that takes the value of the #canvas element once the window is loaded.
+     */
+    static findCanvas() {
+        return new Promise((resolve, reject) => {
+            window.addEventListener("load", resolve)
+        }).then(() => document.getElementById("canvas"));
+    }
+
+    /**
+     * Set camera control vectors in the uniform attributes
+     */
+    setupCameraUniforms(origin, forward, up, right) {
+
+        this.gl.uniform2f(this.gl.getUniformLocation(this.program, 'u_resolution'), this.gl.drawingBufferWidth, this.gl.drawingBufferHeight);
+
+        this.gl.uniform3fv(this.gl.getUniformLocation(this.program, 'u_cameraOrigin'), origin);
+        this.gl.uniform3fv(this.gl.getUniformLocation(this.program, 'u_cameraForward'), forward);
+        this.gl.uniform3fv(this.gl.getUniformLocation(this.program, 'u_cameraUp'), up);
+        this.gl.uniform3fv(this.gl.getUniformLocation(this.program, 'u_cameraRight'), right);
+    }
+
+    render() {
+        this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
+        //requestAnimationFrame(render);
+    }
+
+    async main() {
+        let vertexShaderPromise = downloadFile("shader.vert");
+        let fragmentShaderPromise = downloadFile("shader.frag");
+
+        const canvas = await Pathtrace.findCanvas();
+        this.gl = canvas.getContext('webgl');
+
+        const vertexShader = this.createShader(this.gl.VERTEX_SHADER, await vertexShaderPromise);
+        const fragmentShader = this.createShader(this.gl.FRAGMENT_SHADER, await fragmentShaderPromise);
+
+        this.program = this.createProgram([vertexShader, fragmentShader]);
+
+        const resolutionUniformLocation = this.gl.getUniformLocation(this.program, 'u_resolution');
+
+        this.createVertexPositions();
+
+        this.gl.viewport(0, 0, this.gl.drawingBufferWidth, this.gl.drawingBufferHeight);
+        this.gl.clearColor(0, 0, 0, 0);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+
+        this.gl.useProgram(this.program);
+
+        const cameraParams = Pathtrace.calculateCamera(
+            [0, 1, -0.1],
+            1.5,
+            1 / Math.min(this.gl.drawingBufferWidth, this.gl.drawingBufferHeight)
+        );
+
+        this.setupCameraUniforms([0, 0, 1.8], ...cameraParams);
+
+        this.render();
+    }
+
 }
 
-/**
- * Set camera control vectors in the uniform attributes
- */
-function setupCameraUniforms(gl, program, origin, forward, up, right) {
-
-    gl.uniform2f(gl.getUniformLocation(program, 'u_resolution'), gl.drawingBufferWidth, gl.drawingBufferHeight);
-
-    gl.uniform3fv(gl.getUniformLocation(program, 'u_cameraOrigin'), origin);
-    gl.uniform3fv(gl.getUniformLocation(program, 'u_cameraForward'), forward);
-    gl.uniform3fv(gl.getUniformLocation(program, 'u_cameraUp'), up);
-    gl.uniform3fv(gl.getUniformLocation(program, 'u_cameraRight'), right);
-}
-
-function render(gl) {
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-    //requestAnimationFrame(render);
-}
-
-async function main() {
-    let vertexShaderPromise = downloadFile("shader.vert");
-    let fragmentShaderPromise = downloadFile("shader.frag");
-
-    const canvas = await findCanvas();
-    const gl = canvas.getContext('webgl');
-
-    const vertexShader = createShader(gl, gl.VERTEX_SHADER, await vertexShaderPromise);
-    const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, await fragmentShaderPromise);
-    const program = createProgram(gl, [vertexShader, fragmentShader]);
-
-    const resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
-
-    createVertexPositions(gl, program);
-
-    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    gl.useProgram(program);
-
-    cameraParams = calculateCamera(
-        [0, 1, -0.1],
-        1.5,
-        1 / Math.min(gl.drawingBufferWidth, gl.drawingBufferHeight)
-    );
-
-    setupCameraUniforms(gl, program, [0, 0, 1.8], ...cameraParams);
-
-    render(gl);
-}
-
-main();
+pt = new Pathtrace();
+pt.main();
